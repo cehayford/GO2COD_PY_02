9b2daf0c75d3f07ef5eb0c3330237c185cc495d8
cehayford <sean.alpha18@gmail.com>
2024-11-06 02:23:02 +0000

    change

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'scrappy_db',
         'USER': 'scrappy_db_zq1j_user',
         'PASSWORD': 'SdkTrIItrh8kw4wucMaDMbm1srj2B6ip',
-        'HOST': 'localhost',
+        'HOST': '0.0.0.0',
         'PORT': '5432',
 
         # 'NAME': BASE_DIR / 'db.sqlite3',
